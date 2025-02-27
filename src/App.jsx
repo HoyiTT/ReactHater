baead337083c4b307e17d7e9f887a7df4269e486
HoyiTT <pjs9177@naver.com>
2025-02-27 16:57:04 +0900

    [feat] 2-6 컴포넌트 만들기(state)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-function App() {
+import{useState} from 'react';
 
-  return (
-  <div>
-
-  </div>
+function Counter() {
+  const [count, setCount] = useState(0);
+  return(
+    <div> 
+      State : {count}
+      {/* 3번 불러도 1번만 증가한다. state는 고정값을 가지기 떄문.. */}
+      <button onClick={() => {
+        setCount(count + 1);
+        setTimeout(() => {
+          // 0 출력됨
+          alert(count);
+        }, 1000);
+      }}>
+        Update</button>
+    </div>
     
+  )
+  
+
+}
+function App() {
+  const [count, setCount] = useState(0);
+  const handleUpdate = () => {
+    setCount(count + 1);
+  };
+  return (
+  <div>State : {count}
+    <button onClick={handleUpdate}>Update</button>
+    <Counter></Counter>
+  </div>
   );
 };
 
