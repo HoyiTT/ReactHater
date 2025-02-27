62059019a6f42c1d22bed08dbe609bc8d966779b
HoyiTT <pjs9177@naver.com>
2025-02-27 20:21:51 +0900

    fuck 4-3

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,73 @@ import Layout from './components/Layout';
 import Title from './components/Title';
 import Controls from './components/Controls';
 import TodoList from './components/TodoList';
+import { useState, useRef } from 'react';
 function App() {
+  const idRef = useRef(0);
+  const [list, setList] = useState([]);
+  const [filterType, setFilterType] = useState("ALL");
+  const handleChangeFilterType = (type) => {
+    setFilterType(type);
+  }
+
+  const handleSubmit = (value) => {
+    setList(prevList => prevList.concat({
+      id: (idRef.current += 1),
+      text : value,
+      completed: false
+    }));
+  };
+
+  const handleToggle = (id) => {
+    setList(prevList => prevList.map(item => {
+      if(item.id === id){
+        return {...item, completed: !item.completed};
+      }
+      return item;
+    }));
+};
+
+const handleToggleAll = (flag) => {
+  setList(prevList => prevList.map(item => ({...item, completed: flag})));
+};
+
+const handleDelete = (id) => {
+  setList(prevList => prevList.filter(item => item.id !== id));
+};
+
+
+const handleDeleteCompleted = () => {
+  setList(prevList => prevList.filter(item => !item.completed));
+};
+
+const handleUpdate = (id, text) => {
+  setList(prevList => prevList.map(item => {
+    if(item.id === id){
+      return {...item, text};
+    }
+    return item;
+  }));
+}
+
+const filteredList = list.filter(item => {
+  if(filterType === "ALL") return item;
+  else if(filterType === "TODO") return !item.completed;
+  else if(filterType === "COMPLETED") return item.completed;
+});
+
+
+
   return <div>
     <Layout>
       <Title/>
-      Hello world!
-      <Controls/>
-      <TodoList/>
+      <Controls filterType={filterType} onChangeFilterType = {handleChangeFilterType} onSubmit={handleSubmit}/>
+      <TodoList data={filteredList}
+      onToggle={handleToggle} 
+      onToggleAll={handleToggleAll}
+      onDelete = {handleDelete}
+      onDeleteCompleted={handleDeleteCompleted}
+      onUpdate={handleUpdate}
+      />
     </Layout>
   </div>
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,41 @@
 import TodoItem from "./Todoitem";
 import "./TodoList.css";
-function TodoList() {
-    return<div className="todo-list">
-        <div className="todo-header">
-            <input type="checkbox"  className="todo-checkbox"/>
-            <p className="todo-header-text">할일</p>
-            <button className="todo-header-button">삭제</button>
-        </div>
-        <div>
-            <TodoItem/>
-        </div>
-    
 
+function TodoList({data, onToggle, onToggleAll, onDelete, onDeleteCompleted, onUpdate}) {
+    const isAllCompleted = data.length > 0 &&  data.every((item) => item.completed);
+    const completedCount = data.filter(item => item.completed).length  
+    return(
+        <div className="todo-list">
+            <div className="todo-header">
+                <input type="checkbox"  
+                className="todo-checkbox" 
+                checked={isAllCompleted}
+                onChange={(e) => onToggleAll(e.target.checked)}/>
+                <p className="todo-header-text">할일</p>
+                {
+                    completedCount > 0 && (
+                        <button className="todo-header-button" onClick={onDeleteCompleted}> 
+                            {completedCount}개 선택 삭제
+                        </button>
+                    )
+                }
+                
+            </div>
+        <div>
+            {data.map((item) => (
+                <TodoItem 
+                    id = {item.id}
+                    text={item.text} 
+                    completed={item.completed}
+                    onToggle={() => onToggle(item.id)}
+                    onDelete = {() => onDelete(item.id)}
+                    onUpdate={onUpdate}
+
+                />
+            ))}
+        </div>
     </div>
+    )
 }
 
 export default TodoList;
